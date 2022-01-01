b714e719116a84fdf96c563b0d1442888fd74479
ayhanunal <unalayhan@yahoo.com>
2022-01-01 21:19:57 +0300

    create spring project

---
+++ src/main/java/com/ayhanunal/keywordsmobilebackend/KeywordsMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayhanunal.keywordsmobilebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordsMobileBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeywordsMobileBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayhanunal/keywordsmobilebackend/KeywordsMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayhanunal.keywordsmobilebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordsMobileBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
